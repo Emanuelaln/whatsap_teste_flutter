27a477166768f1e6eaccd3dc576e912d2e0ade90
Emanuel Disthambo Djuma <emanueltchambo@gmail.com>
2023-06-25 18:26:08 +0100

    Perfil e rotas

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:whatsapp_messenger/common/temas/tema_branco.dart';
 import 'package:whatsapp_messenger/common/temas/tema_dark.dart';
 import 'package:whatsapp_messenger/feature/auth/pages/usuario_info_page.dart';
+import 'package:whatsapp_messenger/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
